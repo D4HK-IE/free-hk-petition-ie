eca46ad1eff1cb8eb6d94507ed59811dca5e792f
D4HK-IE <d4hkie@gmail.com>
2019-09-23 22:36:17 +0100

    Updates

---
+++ precache-manifest.a73f227d3221533bd1fb25fb4291130a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb02648f0a3c8bc0b7149453a2ec813",
+    "url": "/free-hk-petition-ie/index.html"
+  },
+  {
+    "revision": "72d3c086eef52fed8792",
+    "url": "/free-hk-petition-ie/static/css/main.645170f1.chunk.css"
+  },
+  {
+    "revision": "a5bcb43c4ae694b22820",
+    "url": "/free-hk-petition-ie/static/js/2.4b54d2b2.chunk.js"
+  },
+  {
+    "revision": "72d3c086eef52fed8792",
+    "url": "/free-hk-petition-ie/static/js/main.0cced963.chunk.js"
+  },
+  {
+    "revision": "e3e1e76d4635c8322e7f",
+    "url": "/free-hk-petition-ie/static/js/runtime~main.eb0e1e01.js"
+  },
+  {
+    "revision": "308385835f6a95da0097be77dae7a875",
+    "url": "/free-hk-petition-ie/static/media/swhk.bg-cropped.30838583.jpeg"
+  }
+]);
