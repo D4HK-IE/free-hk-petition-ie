1d92ec03716e5ed859889632911c4dc5221fd753
Matthew Wong <matthew1245@gmail.com>
2019-09-29 22:29:35 +0100

    Adding GA tracking when user clicks on email link

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+/** Google Analytics tracking. */
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-148981973-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
